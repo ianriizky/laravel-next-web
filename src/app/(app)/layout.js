29e5c8fa46e0cd25c73c08266ea29e72224176ce
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2024-06-14 20:50:30 +0700

    Initial commit

---
+++ src/app/(app)/layout.js
@@ -0,0 +1,23 @@
+'use client'
+
+import Loading from '@/app/(app)/Loading'
+import Navigation from '@/app/(app)/Navigation'
+import { useAuth } from '@/hooks/auth'
+
+const AppLayout = ({ children }) => {
+  const { user } = useAuth({ middleware: 'auth' })
+
+  if (!user) {
+    return <Loading />
+  }
+
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navigation user={user} />
+
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default AppLayout
---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,26 @@
+import AuthCard from '@/app/(auth)/AuthCard'
+import ApplicationLogo from '@/components/ApplicationLogo'
+import Link from 'next/link'
+
+export const metadata = {
+  title: 'Laravel',
+}
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <div className="text-gray-900 antialiased">
+        <AuthCard
+          logo={
+            <Link href="/">
+              <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
+            </Link>
+          }>
+          {children}
+        </AuthCard>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
